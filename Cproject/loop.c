4646c923aadc0158d58a90507a84ae0ccc743b35
kimhsr <kimhsr@naver.com>
2022-04-10 20:58:27 +0900

    기초 프로젝트 2

--- Cproject/loop.c
+++ Cproject/loop.c
@@ -90,32 +90,58 @@ int main(void)
   // ***
   // ****
   // *****
-  for (int i = 0; i < 5; i++)
-  {
-    for (int j = 0; j <= i; j++)
-    {
-      printf("*");
-    }
-    printf("\n");
-  }
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   for (int j = 0; j <= i; j++)
+  //   {
+  //     printf("*");
+  //   }
+  //   printf("\n");
+  // }
   //     *
   //    **
   //   ***
   //  ****
   // *****
 
-  for (int i = 0; i < 5; i++)
+  // for (int i = 0; i < 5; i++)
+  // {
+  //   for (int j = i; j < 5 -1; j++)
+  //   {
+  //     printf(" ");
+  //   }
+  //   for (int k = 0; k <= i; k++)
+  //   {
+  //     printf("*");
+  //   }
+  //   printf("\n");
+  // }
+
+  // 피라미드를 쌓아라 프로젝트
+
+  //     *
+  //    ***
+  //   *****
+  //  *******
+  // *********
+
+  int floor;
+  printf("몇 층으로 쌓겠느냐?");
+  scanf("%d", &floor);
+  for (int i = 0; i < floor; i++)
   {
-    for (int j = i; j < 5 -1; j++)
+    for (int j = i; j < floor - 1; j++)
     {
       printf(" ");
     }
-    for (int k = 0; k <= i; k++)
+    for (int k = 0; k < i * 2 + 1; k++)
     {
       printf("*");
     }
     printf("\n");
+
   }
+
 
 return 0;
 }
