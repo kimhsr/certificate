2ecfdb1d39da1fa5715d0943f12a0ad20a031285
kimhsr <kimhsr@naver.com>
2022-04-11 19:23:42 +0900

    switch case

--- Cproject/condition.c
+++ Cproject/condition.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
 int main(void)
 {
@@ -63,17 +65,69 @@ int main(void)
   // }
 
   // && ||
-  int a = 10;
-  int b = 10;
-  int c = 12;
-  int d = 13;
-  if (a == b || c == d)
+  // int a = 10;
+  // int b = 10;
+  // int c = 12;
+  // int d = 13;
+  // if (a == b || c == d)
+  // {
+  //   printf("a 와 b, 혹은 c 와 d 도 같습니다.\n");
+  // }
+  // else
+  // {
+  //   printf("값이 서로 다르네요.\n");
+  // }
+
+  // 가위 0 바위 1 보 2
+  // srand(time(NULL));
+  // int i = rand() % 3; // 0 ~ 2 반환
+  // if (i == 0)
+  // {
+  //   printf("가위\n");
+  // }
+  // else if (i == 1)
+  // {
+  //   printf("바위\n");
+  // }
+  // else if (i == 2)
+  // {
+  //   printf("보\n");
+  // }
+  // else
+  // {
+  //   printf("몰라요\n");
+  // }
+
+  // srand(time(NULL));
+  // int i = rand() % 3; // 0 ~ 2 반환
+  // // int i = 1;
+  // switch (i)
+  // {
+  //   case 0:printf("가위\n"); break;
+  //   case 1:printf("바위\n"); break;
+  //   case 2:printf("보\n"); break;
+  //   default:printf("몰라요\n"); break;
+  // }
+
+  // 초등학생(8~13) / 중학생(14~16) / 고등학생(17~19)으로 나누면?
+  // if / else if / else
+  int age = 17;
+  switch (age)
   {
-    printf("a 와 b, 혹은 c 와 d 도 같습니다.\n");
-  }
-  else
-  {
-    printf("값이 서로 다르네요.\n");
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+    case 12:
+    case 13:printf("초등학생입니다.\n"); break;
+    case 14:
+    case 15:
+    case 16:printf("중학생입니다.\n"); break;
+    case 17:
+    case 18:
+    case 19:printf("고등학생입니다.\n"); break;
+    default:printf("학생이 아닙니다.\n"); break;
+
   }
 
   return 0;
