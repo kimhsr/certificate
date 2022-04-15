b511e45884d00a9cb38d660eeed7982337af03b3
kimhsr <kimhsr@naver.com>
2022-04-16 02:46:53 +0900

    구조체, typedef

--- Cproject/struct.c
+++ Cproject/struct.c
@@ -5,7 +5,18 @@ struct GameInfo {
   int year;
   int price;
   char *company;
+
+  struct GameInfo *friendGame; // 연관 업체 게임
 };
+
+// typedef struct {
+//   char *name;
+//   int year;
+//   int price;
+//   char *company;
+
+//   struct GameInfo *friendGame; // 연관 업체 게임
+// } GAME_INFO;
 
 int main(void)
 {
@@ -47,7 +58,7 @@ int main(void)
 
   // 구조체를 배열처럼 초기화
   struct GameInfo gameinfo2 = {"봉덕게임", 2017, 100, "봉덕회사"};
-  printf("-- 게임 출시 정보 --\n");
+  printf("\n\n-- 게임 출시 정보 --\n");
   printf("  게임명    : %s\n", gameinfo2.name);
   printf("  발매년도  : %d\n", gameinfo2.year);
   printf("  가격      : %d\n", gameinfo2.price);
@@ -58,16 +69,49 @@ int main(void)
     {"덕봉게임", 2017, 50, "덕봉회사"},
     {"봉덕게임", 2017, 100, "봉덕회사"}
   };
-  printf("-- 게임 출시 정보 --\n");
-  printf("  게임명    : %s\n", gameArray[2]);
-  printf("  발매년도  : %d\n", gameArray[2]);
-  printf("  가격      : %d\n", gameArray[2]);
-  printf("  제작사    : %s\n", gameArray[2]);
 
-  
   // // 구조체 포인터
-  // struct GameInfo *gamePtr; // 미션맨
-  // gamePtr = 
+  struct GameInfo *gamePtr; // 미션맨
+  gamePtr = &gameinfo1;
+  // printf("\n\n-- 미션맨의 게임 출시 정보 --\n");
+  // printf("  게임명    : %s\n", (*gamePtr).name);
+  // printf("  발매년도  : %d\n", (*gamePtr).year);
+  // printf("  가격      : %d\n", (*gamePtr).price);
+  // printf("  제작사    : %s\n", (*gamePtr).company);
+  printf("\n\n-- 미션맨의 게임 출시 정보 --\n");
+  printf("  게임명    : %s\n", gamePtr->name);
+  printf("  발매년도  : %d\n", gamePtr->year);
+  printf("  가격      : %d\n", gamePtr->price);
+  printf("  제작사    : %s\n", gamePtr->company);
+
+  // 연관 업체 게임 소개
+  gameinfo1.friendGame = &gameinfo2;
+  printf("\n\n-- 연관 업체의 게임 출시 정보 --\n");
+  printf("  게임명    : %s\n", gameinfo1.friendGame->name);
+  printf("  발매년도  : %d\n", gameinfo1.friendGame->year);
+  printf("  가격      : %d\n", gameinfo1.friendGame->price);
+  printf("  제작사    : %s\n", gameinfo1.friendGame->company);
+
+  // // typedef
+  // // 자료형에 별명 지정
+  // int i = 1;
+  // typedef int 정수;
+  // typedef float 실수;
+  // 정수 정수변수 = 3; // int i = 3;
+  // 실수 실수변수 = 3.23f; // float f = 3.23f;
+  // printf("정수변수 : %d, 실수변수 %.2f\n\n", 정수변수, 실수변수);
+
+  // typedef struct GameInfo 게임정보;
+  // 게임정보 game1;
+  // game1.name = "한글 게임";
+  // game1.year = 2015;
+  
+  // GAME_INFO game2;
+  // game2.name = "한글 게임2";
+  // game2.year = 2014;
+
+  // // struct GameInformation game3;
+  // // game3.name = "한글 게임3";
 
   return 0;
 }
