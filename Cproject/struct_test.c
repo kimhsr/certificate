3f3f246e944fa01b07104f098c6910baf486a834
kimhsr <kimhsr@naver.com>
2022-04-16 04:19:03 +0900

    정보처리기사 문제

---
+++ Cproject/struct_test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+struct jsu {
+  char nae[12];
+  int os, db, hab, hhab;
+
+};
+
+int main() {
+  struct jsu st[3] = { {"데이터1", 95, 88}, {"데이터2", 84, 91}, {"데이터3", 86, 75} };
+  struct jsu* p;
+  p = &st[1];
+  // (p + 1)->hab = (p + 1)->os + (p + 2)->db;
+  p->hab = p->os + (p+1)->db;
+  // (p + 1)->hhab = (p + 1)->hab + p->os + p->db;
+  p->hhab = p->hab + (p - 1)->os + (p - 1)->db;
+  // printf("%d", (p + 1)->hab + (p + 1)->hhab);
+  printf("%d", p->hab + p->hhab);
+}
