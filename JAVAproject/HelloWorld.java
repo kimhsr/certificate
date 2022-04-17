8c015179ebb51e3c023a343969316734283fde6c
kimhsr <kimhsr@naver.com>
2022-04-18 02:53:03 +0900

    println

---
+++ JAVAproject/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+  public static void main(String[] args) { // 메인 함수이며, 우선으로 실행되는 메소드
+    System.out.println("정보처리기사 자바 공부"); // 로그 출력.
+    System.out.println("asdasdasd");
+
+  }
+
+}
