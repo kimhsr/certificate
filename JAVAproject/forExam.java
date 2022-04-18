d83425e92d976186661e87188a4b496a42912a4a
kimhsr <kimhsr@naver.com>
2022-04-18 15:46:29 +0900

    for문

---
+++ JAVAproject/forExam.java
@@ -0,0 +1,16 @@
+public class forExam {
+  
+  public static void main(String[] args) {
+    
+    String str[] = {"하나", "둘", "셋"};
+
+    for(int i=1; i<3; i++) {
+      System.out.println("정보처리기사");
+    }
+    System.out.println("반갑습니다.");
+
+    for(int i=0; i<str.length; i++){
+      System.out.println("안녕하세요.");
+    }
+  }
+}
