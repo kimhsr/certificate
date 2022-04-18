e9bb69bb7cdf9331f39ea0694e75c5bf36344b08
kimhsr <kimhsr@naver.com>
2022-04-18 15:52:36 +0900

    while문

---
+++ JAVAproject/whileExam.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class whileExam {
+  
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+    while(i < 10){
+      System.out.println(i);
+      i++;
+    }
+
+  }
+}
