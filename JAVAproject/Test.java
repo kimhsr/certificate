9c301f3fcdda87d17562b66d57f198048625309b
kimhsr <kimhsr@naver.com>
2022-04-19 23:06:34 +0900

    접근 제한자 수정

--- JAVAproject/Test.java
+++ JAVAproject/Test.java
@@ -1,7 +1,7 @@
 public class Test {
 
   // defalut, public, private, protected
-  public String a = "정보처리기사";
+  public String a = "정보처리기사"; // 수정
   public static void main(String[] args) {
     
   }
