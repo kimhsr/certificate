6ed58040aefd8d0173f807915d8c0a0e2fff73e4
kimhsr <kimhsr@naver.com>
2022-04-30 23:02:18 +0900

    기사 실기 문제

---
+++ Cproject/28.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sub(int i, int j) {
+  return i - j;
+}
+
+int add(int i, int j) {
+  return i + j;
+}
+
+int main(void) {
+  int i, j, result;
+  int (*pf)(int, int);
+  scanf("%d, %d", &i, &j);
+
+  pf = add;
+  result = pf(i, j);
+  printf("%d, ", result);
+
+  pf = sub;
+  result = pf(i, j);
+  printf("%d", result);
+}
