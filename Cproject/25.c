d0fbfcef24ed591baa23a4bf7fb6fc7e89f1c04d
kimhsr <kimhsr@naver.com>
2022-04-30 19:04:39 +0900

    기사 실기 문제

---
+++ Cproject/25.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int factorial(int n);
+
+int main(void){
+  int (*pf)(int);
+  pf = factorial;
+  printf("%d", pf(3));
+}
+
+int factorial(int n) {
+  if (n <= 1)
+    return 1;
+  else
+    return n * factorial(n - 1);
+}
