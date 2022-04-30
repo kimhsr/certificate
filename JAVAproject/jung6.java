9afc197a348532d16774cc97f05b0b7f1232a1fe
kimhsr <kimhsr@naver.com>
2022-05-01 02:02:32 +0900

    기사 실기 문제

---
+++ JAVAproject/jung6.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class jung6 {
+  public static void main(String[] args) {
+    int i;
+    Scanner s = new Scanner(System.in);
+    System.out.print("숫자를 입력하시오: ");
+    i = s.nextInt();
+    recursive(i);
+    s.close();
+  }
+
+  static int recursive(int n) {
+    int i;
+    if (n < 1)
+      return 2;
+    else {
+      i = (2 * recursive(n - 1)) + 1;
+      System.out.println(i);
+      return i;
+    }
+  }
+}
