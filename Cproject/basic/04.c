d30d09a8c1f6a9e61f730ccaadef489792456ec0
kimhsr <kimhsr@naver.com>
2022-05-16 23:04:58 +0900

    전역변수

--- Cproject/basic/04.c
+++ Cproject/basic/04.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+int num1;
+
 void main(){
-  if(1){
-    int num1 = 10;
-    printf("%d", num1); 
-  } // 여기서 블록의 종료와 함께 메모상에서 소멸
-  printf("%d", num1); // 소멸 됐으니 오류가 발생함
+  num1 = 10;
+  printf("%d", num1);
+  func();
+  printf("%d", num1);
+}
+void func(){
+  num1 = 20;
 }
