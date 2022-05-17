40b01d6db200d3cace7df4ebd58236b663e2aa5a
kimhsr <kimhsr@naver.com>
2022-05-17 21:39:04 +0900

    논리연산자, 증감연산

---
+++ Cproject/basic/08.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int a = 3 + 5, b = 1, c;
+  int ap, bp;
+  ap = a++;
+  bp = ++b;
+  b = 3 * (ap == 8);
+  c = 5 *(ap != 8);
+  printf("%d %d %d %d %d", a, b, c, ap, bp);
+}
