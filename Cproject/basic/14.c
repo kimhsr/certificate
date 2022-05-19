3dab7caa6d00eb717f4aec59f628d316b23a441e
kimhsr <kimhsr@naver.com>
2022-05-19 15:23:29 +0900

    switch문

---
+++ Cproject/basic/14.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+  int a=0, b=1;
+  switch(a){
+    case 0 : printf("%d\n", b++); break;
+    case 1 : printf("%d\n", ++b); break;
+    default : printf("%d\n", b); break;
+  }
+}
