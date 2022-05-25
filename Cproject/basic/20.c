8df41a98684816d98505a1af62db0cd62e8b2e70
kimhsr <kimhsr@naver.com>
2022-05-25 17:15:21 +0900

    배열(1)

---
+++ Cproject/basic/20.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+  char msg[50] = "Hello World! God Luck!";
+  int i = 2, number = 0;
+  while( msg[i] != '!') {
+    if( msg[i] == 'a' || msg[i] == 'e' || msg[i] == 'i' ||
+      msg[i] == 'o' || msg[i] == 'u')
+      number++;
+    i++;
+  }
+  printf("%d", number);
+  return 0;
+}
