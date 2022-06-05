c2950dd2a6fa80da09c198dedebee39d92b32a64
kimhsr <kimhsr@naver.com>
2022-06-05 17:43:42 +0900

    배열포인터

---
+++ Cproject/basic/36.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 3
+int main(void){
+  int (*in)[N], *out, sum=0;
+  in = (int (*)[N]) malloc(N * N * sizeof(int));
+  out = (int *) in;
+  for(int i = 0; i < N * N; i++)
+    out[i] = i;
+  for(int i = 0; i < N; i++)
+    sum += in[i][i];
+  printf("%d", sum);
+}
