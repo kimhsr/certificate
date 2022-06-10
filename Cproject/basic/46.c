40959b46c7b66fc62c33c419ff1ffcd6713edef4
kimhsr <kimhsr@naver.com>
2022-06-10 23:27:19 +0900

    함수에 주소전달 (6)

---
+++ Cproject/basic/46.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  int a,b,c[1];
+  a = 20;
+  b = 20;
+  c[0] = 20;
+  func(&a, b, c);
+  printf("a=%d b=%d c=%d",a, b, c[0]);
+  return 0;
+}
+
+void func(int *a, int b, int *c){
+  int x;
+  x = *a;
+  *a = x++;
+  x = b;
+  b = ++x;
+  --(*c);
+}
