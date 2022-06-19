78833d4a435697ddb6b298a995e1fa9520792cfd
kimhsr <kimhsr@naver.com>
2022-06-20 02:49:39 +0900

    재귀함수 (4)

---
+++ Cproject/basic/56.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int my(int i, int j){
+  if(i<3) i=j=1;
+  else {
+    i = i-1;
+    j = j-i;
+    printf("%d, %d", i, j);
+    return my(i,j);
+  }
+}
+
+void main(){
+  my(5, 14);
+  return 0;
+}
