c396636079875e16d661543e535fda3d93b5b0e5
kimhsr <kimhsr@naver.com>
2022-06-29 22:44:42 +0900

    생성자 & 예외

---
+++ JAVAproject/basic/Ref.java
@@ -0,0 +1,24 @@
+package basic;
+
+class Ref {
+  int a;
+  Ref(int x){
+    a = x;
+  }
+  int sum(Ref obj){
+    int k;
+    k = obj.a-a;
+    a = 10; obj.a = 20;
+    return k;
+  }
+}
+
+class PassRef{
+  public static void main(String args[]){
+    Ref obj1 = new Ref(3);
+    Ref obj2 = new Ref(4);
+    int k1 = obj2.sum(obj1);
+    System.out.print("obj1.a =" + obj1.a);
+    System.out.print("obj2.a =" + obj2.a);
+  }
+}
