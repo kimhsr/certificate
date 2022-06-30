1a705ea43d54a826c93e070cd3a5401eef9a6cc6
kimhsr <kimhsr@naver.com>
2022-07-01 01:37:24 +0900

    생성자 & 예외

---
+++ JAVAproject/basic/constructor.java
@@ -0,0 +1,29 @@
+package basic;
+
+public class constructor {
+  public static void main(String[] args) {
+    try {
+      System.out.println("문장 A");
+      foo();
+      System.out.println("문장 B");
+    }
+    catch(Exception e){
+      System.out.println("문장 C");
+    }
+    System.out.println("문장 D");
+  }
+
+  public static void foo() throws Exception {
+    try {
+      System.out.println("문장 E");
+      throw new Exception();
+    }
+    catch(Exception e){
+      System.out.println("문장 F");
+      throw e;
+    }
+    finally {
+      System.out.println("문장 G");
+    }
+  }
+}
