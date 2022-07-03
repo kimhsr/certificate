4eacc8aeddb2d6264bfbb4772207a02877d4fe9c
kimhsr <kimhsr@naver.com>
2022-07-04 01:10:42 +0900

    접근지정자

---
+++ JAVAproject/basic/A1.java
@@ -0,0 +1,14 @@
+public class A1 {
+  public int x;
+  private int y;
+  protected int z;
+}
+
+public class A2 extends A1 {
+  protected int a;
+  private int b;
+}
+
+public class A3 extends A2 {
+  private int q;
+}
