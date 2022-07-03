975b37c21c724a5fd12ddad7094ab3938c534a8a
kimhsr <kimhsr@naver.com>
2022-07-03 17:34:52 +0900

    접근지정자

---
+++ JAVAproject/basic/Person.java
@@ -0,0 +1,19 @@
+public class Person {
+  private String name;
+  public int age;
+  public void setAge(int age){
+    this.age = age;
+  }
+  public String toString(){
+    return ("name:" + this.name + ", age:" + this.age);
+  }
+}
+
+public class PersonTest{
+  public static void main(String[] args){
+    Person a = new Person();
+    a.setAge(27);
+    a.name = "Gildong";
+    System.out.println(a);
+  }
+}
