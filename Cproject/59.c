f7e2208d9a7c67c2fc74c9bc0852970bc1081337
kimhsr <kimhsr@naver.com>
2022-07-21 17:08:38 +0900

    기사 실기 문제

---
+++ Cproject/59.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#define SIZE 3
+
+int main(void){
+  int num[SIZE] = {1, 3, 6};
+  int a = 10, b = 30;
+  func( num, &a, b );
+  printf("a=%d, b=%d \n", a, b);
+  return 0;
+}
+
+void func(int *m, int *x, int y){
+  int i = 0, n = 0;
+  y = *x;
+  n = *(m+1) + (*m+2);
+  *x = ++n;
+}
